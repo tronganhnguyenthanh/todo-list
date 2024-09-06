c2fd87b295d14f66c7cd3678e71bb00154328404
Phuong Nguyen <tronganhnguyen222@gmail.com>
2024-09-06 23:33:51 +0700

    [UPDATE][FE] confirm delete function

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,12 +13,6 @@ function getTodoList(todos){
       <div class="card mt-2">
         <div class="d-flex justify-content-center">
           <h1 class="text-center text-secondary">${output}</h1>
-          <button class="btn btn-success m-2" onclick="isCompleted('${output}')">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark-heart" viewBox="0 0 16 16">
-              <path fill-rule="evenodd" d="M8 4.41c1.387-1.425 4.854 1.07 0 4.277C3.146 5.48 6.613 2.986 8 4.412z"/>
-              <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z"/>
-            </svg>
-          </button>
           <button class="btn btn-danger m-2" onclick="deleteToDoList()">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
               <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
@@ -33,19 +27,13 @@ function getTodoList(todos){
   document.querySelector("#todoList").innerHTML = todo
 }
 
-function isCompleted(output){
- document.querySelector("#todoList").innerHTML = `
-  <del class="d-flex justify-content-center">
-   <h1 class="text-center text-info">${output}</h1>
-  </del>
- `
-}
-
 document.querySelector(".btn.btn-secondary").addEventListener("click", function(){
  getTodoList(todos)
 })
 
 function deleteToDoList(index){
-  todos.splice(index, 1)
-  getTodoList(todos)
+  if(window.confirm("Are you sure you want to delete this to do ?") === true){
+   todos.splice(index, 1)
+   getTodoList(todos)
+  }
 }
