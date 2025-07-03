de6784a2a9e8e910240016297f732234d7e4a853
Trong Anh Nguyen Thanh <tronganhnguyen222@gmail.com>
2025-07-03 10:50:15 +0700

    [UPDATE][FE] build to do check is completed function

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,13 +29,20 @@ function deleteToDoList(){
     todo += `<div class="col-lg-12 col-md-12 col-sm-12">
      <div class="card mt-2">
        <div class="d-flex justify-content-center">
-         <h1 class="text-center text-info text-truncate">${output.completed ? `<del>${output?.title}</del>` : `<h2 class="text-danger align-middle">${output?.title}</h2>`}</h1>
-         <button class="btn btn-danger m-2" onclick="deleteToDoList()">
+        <input 
+          type="checkbox"
+          class="m-2"
+          style="width:30px"
+          value="${(output.id)}"
+          onchange="onChangeCompleteTask('${output?.title}')"
+        >
+        <h1 class="text-center text-info text-truncate" id="checkCompletedTask">${output.title}</h1>
+        <button class="btn btn-danger m-2" onclick="deleteToDoList()">
            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
              <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
            </svg>
-          </button>
+        </button>
       </div>
     </div>
    </div>
@@ -43,6 +50,16 @@ function deleteToDoList(){
    })
   todo += "</div>"
   document.querySelector("#todoList").innerHTML = todo
+ }
+ 
+ function onChangeCompleteTask(title){
+   let isCompleted = window.confirm("Are you sure that you have completed it?")
+   if(isCompleted === true){
+     document.querySelector("#checkCompletedTask").innerHTML = `<del class="text-danger">${title}</del>`
+   }else{
+     document.querySelector("input").checked = false
+     getTodoList()
+   }
  }
  
  function getSelectTodoList(){
@@ -54,7 +71,9 @@ function deleteToDoList(){
      todos = i?.slice(0, 100)
      let select = "<select class='form-custom-control form-control' onchange='filterTodoList(this.value)'>"
       todos?.forEach(function(i){
-       select += `<option value=${i?.id}>${i?.title}</option>`
+       select += `
+        <option value=${i?.id}>${i?.title}</option>
+       `
       })
      select += "</select>"
      document.querySelector("#filter").innerHTML = select
